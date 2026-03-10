d5769ff663ba63a012238fad92832f4534557b96
AzizReja10 <rejaaziz686@gmail.com>
2026-03-10 12:04:30 +0530

    changes

--- Documents/workspace-spring-tools-for-eclipse-5.0.1.RELEASE/ProjectBlog/src/main/java/com/aziz/config/SecurityConfig.java
+++ Documents/workspace-spring-tools-for-eclipse-5.0.1.RELEASE/ProjectBlog/src/main/java/com/aziz/config/SecurityConfig.java
@@ -19,7 +19,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+            .authorizeHttpRequests(auth -> auth
+            	    .requestMatchers("/api/auth/**").permitAll()  // login/register open
+            	    .requestMatchers("/api/posts").permitAll()    // reading open
+            	    .anyRequest().authenticated()                // everything else needs login
+            	);
         return http.build();
     }
 }
